f502a3efb7e9ea27d8ddc702dd15c36434937e3f
github-user1000 <github_user1000@protonmail.com>
2023-01-16 19:50:58 -0800

    promise section 1

---
+++ app.js
@@ -0,0 +1,12 @@
+// call the fetch API to give the var its value
+const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+
+// remember fetch returns a promise! logging it right after will also tell us what 'state' its in
+console.log(fetchPromise);
+
+// suppose we get success...then fetch is gonna take our response [object] (from the server) and do the function within
+fetchPromise.then((response) => {
+    console.log(`Received response: ${response.status}`);
+});
+
+console.log("Started request…");
