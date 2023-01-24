03f383b3aa03bc74dc117d340c19c93e0eea45a8
github-user1000 <github_user1000@protonmail.com>
2023-01-23 18:40:43 -0800

    added alarm example, use promise chain, then async await

--- app.js
+++ app.js
@@ -26,4 +26,28 @@ fetchPromise
         console.log(data[0].name);
 });
 
+const name = document.querySelector('#name');
+const delay = document.querySelector('#delay');
+const output = document.querySelector('#output');
+const button = document.querySelector('#set-alarm');
 
+function alarm(person, delay) {
+    return new Promise((resolve, reject) => {
+        if(delay < 0) {
+            throw new Error('Alarm delay must not be negative');
+        }
+        setTimeout(() => {
+            resolve(`Wake up, ${person}!`);
+        }, delay);
+    });
+};
+
+button.addEventListener('click', async() => {
+    try {
+        const message = await alarm(name.value, delay.value);
+        output.textContent = message;
+    }
+    catch(error) {
+        output.textContent = `Couldn't set alarm: ${error}`;
+    }
+});
